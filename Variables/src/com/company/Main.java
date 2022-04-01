9a638cebe8e8f8c5a88d4ba99f37449f866bf3d2
Henrique Leote <80213140+henriqueleote@users.noreply.github.com>
2022-04-01 17:38:06 +0100

    Add files via upload

---
+++ Variables/src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String text = "The result is: ";
+        int a = 5;
+        int b = 10;
+        int z = a + b;
+	    System.out.println(text + z);
+    }
+}
